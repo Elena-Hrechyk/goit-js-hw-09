4e9e39a73564ec95045efbf0c5665ad3d86f5cd8
Elena-Hrechyk <117777710+Elena-Hrechyk@users.noreply.github.com>
2023-04-14 03:16:46 +0000

    Deploying to gh-pages from @ Elena-Hrechyk/goit-js-hw-09@3767dd42940e781c0ee59149787a8ab346cc0891 🚀

---
+++ 01-color-switcher.a285d00a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.querySelector("body"),d=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;let n=null;e.disabled=!0,t.addEventListener("click",(function(t){t.target.disabled=!0,e.disabled=!1,n=setInterval((()=>{a.style.backgroundColor=d}),1e3)})),e.addEventListener("click",(function(e){clearInterval(n),e.target.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.a285d00a.js.map
